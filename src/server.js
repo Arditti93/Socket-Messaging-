93cdb8f400bd447b7afbb1551a75946b3e7928bf
Arditti93 <alex.arditti@hotmail.com>
2022-08-02 14:13:09 +0100

    feat: added socketControllers, started listConnections

--- src/server.js
+++ src/server.js
@@ -11,54 +11,29 @@ var port = process.env.PORT || 3000;
 app.use(express.json()); 
 app.use(userRouter)
 
-const Msg = require("./models/models") 
-const Connections = require("./models/connectionModel") 
+const { addConnection, removeConnection, listConnections, saveMsg } = require("./socketControllers")
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 }); 
 
 io.on('connection', (socket) => {
-    console.log('a user connected')
-    let username = "Placeholdern"
+    console.log("connect " + socket.id)
+    let username = "Placeholder"
     addConnection(socket.id, username )
+    listConnections()
 
-    socket.on('chat message', function(msg){
+    socket.on('chat message', function(msg){ß
       io.emit('chat message', msg);
-      saveMsg(msg)
+      saveMsg(msg, socket.id, username)
     });
 
     socket.on('disconnect', () => {
-    console.log('user disconnected');
+      console.log("disconnect " + socket.id)
+      removeConnection(socket.id)
     });
   });
 
 http.listen(port, function(){
   console.log('listening on *:3000');
 }); 
-
-async function addConnection (connectionId) {
-  let connection = {
-    "connectionId": connectionId,
-    "username": "placeholder"
-  } 
-  try{
-    const newConnection = await Connections.create(connection)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-async function saveMsg (msg){  
-  let msgObj = {
-    //TODO: Store connectionId and username
-    "msg": msg
-  }
-  try {
-    const newMsg = await Msg.create(msgObj);
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-//TODO: remove connections
---
+++ src/socketControllers/index.js
@@ -0,0 +1,50 @@
+const Msg = require("../models/models") 
+const Connections = require("../models/connectionModel")
+
+exports.addConnection = async (connectionId) => {
+    let connection = {
+      "connectionId": connectionId,
+      "username": "Placeholder"
+    } 
+    try{
+      const newConnection = await Connections.create(connection)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+exports.removeConnection = async (connectionId) => {
+    try{
+        await Connections.deleteOne({connectionId: connectionId})
+    } catch (error) {
+        console.log(erorr)
+    }
+}
+  
+exports.saveMsg = async (msg, socketId, username) =>{  
+    let msgObj = {
+      "msg": msg,
+      "username": username,
+      "connectionId": socketId
+    }
+    try {
+      const newMsg = await Msg.create(msgObj);
+    } catch (error) {
+      console.log(error)
+    }
+} 
+
+exports.listConnections = async () => {
+    let output
+    try {
+        const listUsers = await Connections.find({});
+        output = listUsers
+        console.log("in try")
+    } catch (error) {
+        output  = error
+        console.log("in catch")
+    } finally {
+        console.log(output)
+        return output
+    }
+}
